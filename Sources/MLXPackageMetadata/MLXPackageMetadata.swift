e1ff8025c59ec7c5c6016bf56b94384a88252e48
Gerardo Grisolini <gerardo.grisolini@gmail.com>
2026-06-12 08:57:11 +0200

    v0.3.3

--- Sources/MLXPackageMetadata/MLXPackageMetadata.swift
+++ Sources/MLXPackageMetadata/MLXPackageMetadata.swift
@@ -7,7 +7,7 @@ public enum MLXPackageMetadata {
     public static let packageName = "mlx-coder"
     public static let coderExecutableName = "mlx-coder"
     public static let serverExecutableName = "mlx-server"
-    public static let version = "0.3.2"
+    public static let version = "0.3.3"
 
     public static func versionDescription(for executableName: String) -> String {
         "\(executableName) \(version)"
